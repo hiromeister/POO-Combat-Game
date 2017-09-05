952f9a79ffba036370c9694b857bd01ccfb5f010
Robin <hermandrobin@gmail.com>
2017-09-05 13:27:36 +0400

    Etape1OK

--- js/hero.js
+++ js/hero.js
@@ -5,10 +5,41 @@ function heroCard(name, health, armor, damage){
     this.damage = damage;
 }
 
+function minionsCard(name, health, damage){
+    this.name = name;
+    this.health = health;
+    this.damage = damage;
+}
+
+
+// Les objets
+    // Héro 
+        // Gérald        
 let gerald = new heroCard('Gerald', 200, 0, 15);
 console.log(gerald);
-console.log(gerald.health);
 
-// Ajout +50 pts
+
+// Méchants
+    // Minions
+        // Minion
+let minion = new minionsCard('minion', 10, 10);
+        // Lieutenant minion
+let lieutenantMinion = new minionsCard('Minion Lieutenant', 30, 30);
+        // Chef Minion
+let chefMinion = new minionsCard('Chef Minion', 100, 100);
+
+
+
+// HP Bonus
+console.log("Gerald a " + gerald.health + " " + "points de vie.");
 gerald.health = gerald.health+50;
-console.log(gerald.health);
+console.log("Gerald a pris un bonus de vie supplémentaire et a maintenant" + " " + gerald.health + " " + "points de vie.");
+
+// Armor Bonus
+console.log(gerald.armor)
+console.log("Gerald a " + gerald.armor + " " + "points d'armure.");
+gerald.armor = gerald.armor+20;
+console.log("Gerald obtient une armure et a maintenant" + " " + gerald.armor + " " + "points d'armure.");
+
+//Minion attack
+console.log("Un minion attaque Gérald");
